229d74fbbad4e9b733cf1f5f3dcd56d6f3b28757
sagor <sagor.onetouch@gmail.com>
2024-12-05 18:14:10 +0600

    Template driven forms, Footer texture design

--- src/app/free-trial/free-trial.component.ts
+++ src/app/free-trial/free-trial.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-free-trial',
   standalone: true,
   imports: [
     RouterLink,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './free-trial.component.html',
   styleUrl: './free-trial.component.css'
@@ -15,9 +17,9 @@ import { Router, RouterLink } from '@angular/router';
 export class FreeTrialComponent {
   constructor(private router: Router) {
   }
-  
+
   backToHome(): void {
-    this.router.navigate(['/primary-header']); // Navigate to HeaderComponent
+    this.router.navigate(['/primary-header']);
   }
 
   selectedService: string | null = null;
@@ -25,6 +27,20 @@ export class FreeTrialComponent {
     this.selectedService = this.selectedService === value ? null : value;
   }
 
-  
+  // define form model
+  formData = {
+    name: '',
+    businessEmail: '',
+    companyName: '',
+    phone: '',
+    selectedService: null
+  };
+
+  // handle form submission
+  onSubmit(): void {
+    console.log('Form Submitted!', this.formData);
+  }
+
+
 }
 
